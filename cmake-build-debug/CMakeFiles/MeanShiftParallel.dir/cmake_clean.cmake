9771aec0499f5166a76f55ac510d56a631761c07
FranciAccia <acciaiolifrancesco3@gmail.com>
2025-12-16 13:41:29 +0100

    develop first commit

---
+++ cmake-build-debug/CMakeFiles/MeanShiftParallel.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MeanShiftParallel.dir/main.cpp.o"
+  "CMakeFiles/MeanShiftParallel.dir/main.cpp.o.d"
+  "MeanShiftParallel"
+  "MeanShiftParallel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MeanShiftParallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
